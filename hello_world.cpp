65704b281e12f19237b449a187bd69f8f20020ff
GrafininV <gravla777mail.ru>
2026-05-29 02:45:18 +0400

    Изменить code style на Mozilla style, переименовать переменную

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,13 +4,10 @@
 // Main function of the program
 int main() {
     std::string name; // Variable for storing the user's name
-
     // Ask user for name
     std::cout << "Enter your name: ";
     std::cin >> name; // Read the name
-
     // Print greeting with name
     std::cout << "Hello world from " << name << std::endl;
-
     return 0;
 }
