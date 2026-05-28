8b11159b06698ea429bae356023c5305f7e6af11
GrafininV <gravla777mail.ru>
2026-05-28 15:39:52 +0400

    Избавляемся от using namespase std, добавляем префикс std::

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <string>
-using namespace std;
 int main() {
-string name;
-cout << "Enter yor name: ";
-cin >> name;
-cout << "Hello world from" << name << endl;
-return 0;
+    std::string name;
+    std::cout << "Enter yor name: ";
+    std::cin >> name;
+    std::cout << "Hello world from" << name << endl;
+    return 0;
 }
